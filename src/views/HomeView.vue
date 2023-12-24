16acf1fc7f61de62fb7bede18b98c1795fc3b10a
shomurodovX <cometa11we@gmail.com>
2023-12-24 22:38:27 +0500

    header componenti qoshildi

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,11 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div><h1>hello world</h1></div>
 </template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import header from "../components/header.vue";
 export default {
-  name: 'HomeView',
   components: {
-    HelloWorld
-  }
-}
+    header,
+  },
+};
 </script>
