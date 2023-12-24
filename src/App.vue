c5f7af1820d98b064f6658fba576df2b89130bd6
shomurodovX <cometa11we@gmail.com>
2023-12-24 23:07:22 +0500

    font ulandi

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,13 @@
 </template>
 
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Cormorant:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Montserrat:wght@500;600;700;800&display=swap");
+/* font  */
 * {
   padding: 0;
   margin: 0;
   box-sizing: border-box;
+  font-family: "Cormorant", serif;
 }
 li {
   list-style: none;
